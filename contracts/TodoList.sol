e8bee28f7226d299e557ff1aaecea9195b8e82fc
Kenneth <kennethkjliang@gmail.com>
2021-06-25 19:26:31 -0700

    listed out task and added test

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,24 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    createTask("Check out https://kennethliang.com/");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
+
 }
 
